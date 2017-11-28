b0fbf4514af6dccb98bbe75322f5f4e77a77faa5
Pablo Lopez <esviasalpa@gmail.com>
2017-11-28 11:59:49 +0100

    Ejemplo de pruebas unitarias en VisualStudio

---
+++ PruebaUnitarias/AppPruebaUnitariasMatematicas/AppPruebaUnitariasMatematicas/Calculadora.cs
@@ -0,0 +1,66 @@
+﻿/*
+ Autor: Pablo Lopez
+ Ejemplo consola: de pruebas Unitarias tras calcular su complegidad ciclomatica
+ Fecha:23/11/2017
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppPruebaUnitariasMatematicas.pbl
+{
+    //Los metodos se colocaran publicos, para poder probarlos en el proyecto de pruebas "CalculadoraTEST"
+    public class DemasiadosParametrosException : Exception { }
+    public class FaltaParametroException : Exception { }
+    public class NoNumberException : Exception { }
+    public class NumenoNoPositivoException : Exception { }
+    public class NumeroDemasiadoGrandeException : Exception { }
+
+  public class Calculadora
+    {
+       
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static bool EsPrimo(string[] args)
+        {                                        //Señalando caminos basicos:
+            if (args.Length == 0)                   //1                 
+                throw new FaltaParametroException();//E1-> "Excepcion 1 "toda excepcion termina directamente la ejecucion(no es rescatable)     "Lanza una nueva excepcion declarada en esa clase."
+
+            else if (args.Length > 1)               //2
+                throw new DemasiadosParametrosException();//E2
+            else//LOS ELSE NO SON CAMINOS BASICOS
+            {
+                try
+                {                                      
+                    int numero = int.Parse(args[0]);//3
+                    if (numero < 0)                 //4
+                        throw new NumenoNoPositivoException();//E4
+                    else
+                    { 
+                        //Por fin podemos calcular en un entorno seguro
+                                //5     6           9
+                        for (int i = 2; i < numero; i++)
+                        {
+                            if (numero % i == 0)//7
+                                return false;//8
+                        }
+                    }   
+
+                }
+                catch(FormatException)              
+                {
+                    throw new NoNumberException();  //E3
+                }
+            }
+
+            return true;//10
+        }//11 Despues de cada excecion no ira al ture.. si n oa  esata llave de finalizacion del metodo.
+
+    }
+}
---
+++ PruebaUnitarias/AppPruebaUnitariasMatematicas/PruebaUnitCajaBlancaMatematicasTEST/CalculadoraTEST.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//Añadido el espacio de nombre del proyecto que vamos a probar,para poder ver sus metodos"ademas de  colocar dichos metodos como publicos
+using AppPruebaUnitariasMatematicas.pbl;
+
+namespace PruebaUnitCajaBlancaMatematicasTEST
+{
+    /*
+     Primer paso antes de empezar , agregar a las referencias  de el proyecto de PruebaUnitCajaBlancaMatematicasTEST el proyecto a probar
+     (Boton derecho en referencias de "PruebaUnitCajaBlancaMatematicasTEST") y agregar referencia "en la pestaña de solucion" ,y marcamos el proyecto que probaremos con este.
+     * 
+     Segundo paso: Pestaña de PRUEBA  y en ejecutar todas las pruebas.
+     */
+    [TestClass]
+    public class CalculadoraTEST
+    {
+        //Camino1
+        [TestMethod]//Para cara metodo a probar
+        [ExpectedException(typeof(FaltaParametroException))] //Cuando el metodo espera recibir una excepcion tras probarlo y que excepcion recibe.
+        public void C1esPrimo_NULL_ExcepcionE1()
+        {
+          string[] valores = { };//Valor de entrada que vamos a dar para probar el primer camino.
+          Calculadora.EsPrimo(valores);//LLamamos al metodo a probar  
+           
+        }
+
+        //Camino2
+        [TestMethod]//Para cara metodo a probar
+        [ExpectedException(typeof(DemasiadosParametrosException))] //Cuando el metodo espera recibir una excepcion tras probarlo y que excepcion recibe.
+        public void C2esPrimo_ArrayDosParametros_ExceptionE2()
+        {
+            string[] valores = {"5", "8"};//Valor de entrada que vamos a dar para probar el primer camino.
+            Calculadora.EsPrimo(valores);//LLamamos al metodo a probar  
+        }
+
+        //Camino3
+        [TestMethod]//Para cara metodo a probar
+        [ExpectedException(typeof(NoNumberException))] //Cuando el metodo espera recibir una excepcion tras probarlo y que excepcion recibe.
+        public void C3esPrimo_ArraySinNumero_ExceptionE3()
+        {
+            string[] valores = {"a"};//Valor de entrada que vamos a dar para probar el primer camino.
+            Calculadora.EsPrimo(valores);//LLamamos al metodo a probar  
+        }
+
+        //Camino4
+        [TestMethod]//Para cara metodo a probar
+        [ExpectedException(typeof(NumenoNoPositivoException))] //Cuando el metodo espera recibir una excepcion tras probarlo y que excepcion recibe.
+        public void C4esPrimo_ArrayValorNegativo_ExceptionE4()
+        {
+            string[] valores = {"-1"};//Valor de entrada que vamos a dar para probar el primer camino.
+            Calculadora.EsPrimo(valores);//LLamamos al metodo a probar  
+        }
+
+        [TestMethod]//Para cara metodo a probar
+        public void C5esPrimo_DOS_TRUE()
+        {
+            string[] valores = { "2" };//Valor de entrada que vamos a dar para probar el primer camino.
+            //Assert -> para recoger un dato  que vamos a probar que  no va devolver una excepcion  y si cumple el metodo, en este caso termian el metodo y devuelve un true
+            Assert.IsTrue(Calculadora.EsPrimo(valores));//LLamamos al metodo a probar  
+        }
+
+        [TestMethod]//Para cara metodo a probar
+        public void C6esPrimo_CUATRO_FALSE()
+        {
+            string[] valores = { "4" };//Valor de entrada que vamos a dar para probar el primer camino.
+            //Assert -> para recoger un dato  que vamos a probar que  no va devolver una excepcion y si cumple el metodo, en este caso termian el metodo y devuelve un falso
+            Assert.IsFalse(Calculadora.EsPrimo(valores));//LLamamos al metodo a probar  
+        }
+
+
+        [TestMethod]//Para cara metodo a probar
+        public void C7esPrimo_TRES_TRUE()
+        {
+            string[] valores = { "3" };//Valor de entrada que vamos a dar para probar el primer camino.
+            //Assert -> para recoger un dato  que vamos a probar que  no va devolver una excepcion y si cumple el metodo, en este caso termian el metodo y devuelve un falso
+            Assert.IsTrue(Calculadora.EsPrimo(valores));//LLamamos al metodo a probar  
+        }
+
+
+        
+    }
+}
